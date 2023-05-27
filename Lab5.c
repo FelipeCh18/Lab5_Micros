ac45d286a7f5e5de287328c8fe9c6748832969ec
FelipeCh18 <fchaves@unal.edu.co>
2023-05-26 23:01:14 -0500

    Funcional sin variables cambiadas

--- Lab5.c
+++ Lab5.c
@@ -12,72 +12,72 @@
 #define DATA_IN RC2
 #define DATA_OUT LATC2
 
-unsigned char Temperatura, Humedad, Che;
-unsigned char Temperatura_EEPROM;//Para el bonus
+unsigned char Temp, Hum;
+unsigned char TempEEPROM;//Para el bonus
 unsigned int A=0,B= 0;//valor de entrada para seleccionar unidades de temperatura
 
 
 
 //Prototipos de las funciones
-void LeerDHT11(void);
+void LeerHT11(void);
 unsigned char LeerByte(void);
 unsigned char LeerBit(void);
-unsigned char Check();
 void Transmitir(unsigned char);
 unsigned char Recibir(void);
 void TransmitirDatos(unsigned int Ent1, unsigned int Ent2);
 void ColorRGB(unsigned int val);
 unsigned int Conversion(unsigned char);
-void SaveData_EEPROM(unsigned int dir, unsigned char Dato);
-unsigned char ReadData_EEPROM(unsigned int dir);
+void GuardarDatoEnEEPROM(unsigned int dir, unsigned char Dato);
+unsigned char leerDatoEnEEPROM(unsigned int dir);
 
 //void LedPot(void);
 
 void main(void) {
 
     //CONFIGURACION PARA EL RS232
-    OSCCON = 0b01110000; //Establece el reloj interno en 8Mhz
-    __delay_ms(10);
-    TXSTA = 0b00100000; //Configuraci�n del transmisor, habilitaci�n del transmisor y modo asincr�nico, bajas velocidades
-    RCSTA = 0b10010000; //Configuraci�n del receptor, habilitaci�n del modulo EUSART, se habilita el receptor
+    ConfiguraLCD(4);
+    OSCCON = 0b01110110; //Establece el reloj interno en 8Mhz
+    __delay_ms(1);
+    DATA_OUT = 0;
+    TXSTA = 0b00100000; //Configuraci?n del transmisor, habilitaci?n del transmisor y modo asincr?nico, bajas velocidades
+    RCSTA = 0b10010000; //Configuraci?n del receptor, habilitaci?n del modulo EUSART, se habilita el receptor
     BAUDCON = 0b00000000; //Configuracion del modulo adc, no inversion logica, divisor de frecuencia 8bits, modo bajo consumo desactivado,
     //autodeteccion de velocidad off.
-    SPBRG = 12; //Valor para la vel de transmisi�n de datos, revisar formula -> SPBRG = 8M/(64*9600)-1
+    TRISE=0;
+    SPBRG = 12; //Valor para la vel de transmisi?n de datos, revisar formula -> SPBRG = 8M/(64*9600)-1
     //ConfigADC
     ADCON0 = 0b00000001;
-    ADCON1 = 13;
+    ADCON1 = 14;
     ADCON2 = 0b10001000; //Justificacion a derecha, adquisicion instantanea
     //Fin config ADC
     //CONFIGURACION DE PUERTOS I/O
-    TRISB = 0; //Colocar puerto B como salida
+    TRISB = 0b00001110; //Colocar puerto B como salida
     TRISD = 0; //Colocar puerto D como salida
-    TRISA = 0b00000001; //Colocar pines A00 como entrada digital para ADC
-    USBEN = 0;//habilita RC4 y RC5 deshabilitando modulo USB
-    UTRDIS = 1;//Deshabilitar el transceptor USB
-    TRISC = 0b11110110; //Pin C0 salida LED  y C1 como entrada (seleccion entrada) C2- SENSOR, C4, C5-  seleccion unidad  RC6 como entrada TX, para lectura RC7 RX
+    TRISA0 = 1; //Colocar pines A00 como entrada digital para ADC
+    TRISC = 0b11010111; //Colocar Pines C0 y C1 como entrada (seleccion de temperatura) C2- SENSOR, C4-Selector cambio unidad envio datos RC6 como entrada TX, para lectura RC7 RX
+    USBEN = 0;//habilita RC4 y RC5 desabilitando modulo USB
+    UTRDIS = 1;
     RBPU = 0; //Activar resistencias pull up
-    DATA_OUT = 0;
-    Temperatura_EEPROM = ReadData_EEPROM(0);
     
-    ConfiguraLCD(4);
+    TempEEPROM = leerDatoEnEEPROM(0);
+    
     InicializaLCD(); //Funcion para configuracion inicial del LCD
-    
     //Timer0 interrupcion
-    T0CON=0b10000011;//No habilita timer0, 16 bits de resolucion, reloj interno
+    T0CON=0b00000011;//No habilita timer0, 16 bits de resolucion, reloj interno
     TMR0IF=0;// apaga bandera
     TMR0=3036; // valor pre carga
     TMR0IE=1; //Habilita la interrupcion 
     GIE=1; //habilita interrupciones globales
     TMR0ON=1;//Habilita la interrupcion Timer0, primer bit de T0CON
     //Fin de configuracion para Timer0
-   
     BorraLCD(); //Limpiar el LCD
 
-    if (Temperatura_EEPROM != 0xFF) {
+    if (TempEEPROM != 0xFF) {
         MensajeLCD_Var("Ultima temp:");
         DireccionaLCD(192);
-        EscribeLCD_c(Temperatura_EEPROM / 10 + 48);
-        EscribeLCD_c(Temperatura_EEPROM % 10 + 48);
+        EscribeLCD_c(TempEEPROM / 10 + 48);
+        EscribeLCD_c(TempEEPROM % 10 + 48);
+        EscribeLCD_c(223);
         EscribeLCD_c('C');
         __delay_ms(2000);
         BorraLCD();
@@ -85,62 +85,60 @@ void main(void) {
     
     MensajeLCD_Var("Iniciando"); //Escribir mensaje de bienvenida
     __delay_ms(500); //Retraso para evitar errores
-    EscribeLCD_c(46);
+    EscribeLCD_c('.');
     __delay_ms(500);
-    EscribeLCD_c(46);
+    EscribeLCD_c('.');
     __delay_ms(500);
-    EscribeLCD_c(46);
+    EscribeLCD_c('.');
     __delay_ms(500);
     BorraLCD();
+    __delay_ms(500);
     
-    
+    MensajeLCD_Var("Temp:");
+    __delay_ms(500);
+    DireccionaLCD(0xC0);
+    MensajeLCD_Var("Hum:");
     while (1) {
         __delay_ms(500);
         //LATD0=1; LED?
         __delay_ms(500);
         //LATD0=0;
-        LeerDHT11();
+        LeerHT11();
+        __delay_ms(100);
         //IniFlag = 1;
-        SaveData_EEPROM(0, Temperatura);
-        ColorRGB(Temperatura); 
+        GuardarDatoEnEEPROM(0, Temp);
+        __delay_ms(100);
+        ColorRGB(Temp); 
+        __delay_ms(100);
         
-        
-        if(!RC1) TransmitirDatos(RC4, RC5);
+        if(!RC4) TransmitirDatos(RB1, RB2);
         else TransmitirDatos(A, B);
         Conversion(0);
         RB0 = (ADRES <= 511) ? 0 : 1; //2.5*(2^10-1)/5 
     }
 }
 
-void LeerDHT11(void) {
-    unsigned char i, contr = 0;
-    //Se le dan dos pulsos al sensor para que haga la medici�n
-    DATA_DIR = 0;
-    __delay_ms(18);
-    //Pulso 1
-    DATA_DIR = 1;
-    while (DATA_IN == 1);
-    __delay_us(40);
-    if (DATA_IN == 0) contr++;
-    __delay_us(80);
-    //Pulso 2 
-    if (DATA_IN == 1) contr++;
-    while (DATA_IN == 1); //Termina cuando el sensor toma control de la l�nea
-    //Recepci�n de datos
-    /*Humedadedad = LeerByte();
+void LeerHT11(void) {
+    //Por defecto el pin de comunicaci?n est? en alto, para iniciar la comunicaci?n se debe poner la l?nea de datos en bajo durante 18ms
+    DATA_DIR = 0; //Configura el pin como salida, por defecto su valor de salida es 0
+    __delay_ms(18); //Se esperan los 18ms
+    DATA_DIR = 1; //Se reestablece el pin a entrada digital
+    //Ahora se espera la respuesta del sensor
+    while (DATA_IN == 1); //Tiempo en alto mientras el sensor responde
+    __delay_us(120); //Pulso bajo, respuesta del sensor 80us, posteriormente pulso en alto de una duraci?n similar.
+    while (DATA_IN == 1); //Tiempo en alto que dura hasta que el sensor toma control del canal de comunicaci?n
+    //Recepci?n de datos
+    Hum = LeerByte();
     LeerByte();
-    Temperatura = LeerByte();
+    Temp = LeerByte();
     LeerByte();
-    Che = LeerByte();*/
-    Humedad = 0;
-    Temperatura = 42;
-    Che = 0;
+    LeerByte();
 }
 
 unsigned char LeerByte(void) {
     unsigned char res = 0, i;
     for (i = 8; i > 0; i--) {
-        res = (res << 1) | LeerBit(); //Va moviendo los digitos del byte a la derecha y a�adiendo los valores le�dos
+        res = (res << 1) | LeerBit(); //Va moviendo los digitos del byte a la izquierta y a?adiendo los valores le?dos
     } //Comienza 00000000, lee 1, entonces 0000001, lee 0, entonces 00000010, lee 1, entonces 00000101, hasta llenar el byte
     return res;
 }
@@ -158,13 +156,6 @@ unsigned char LeerBit(void) {
     return res;
 }
 
-unsigned char Check(void) {
-    unsigned char res = 0, aux;
-    aux = Temperatura + Humedad;
-    if (aux == Che) res = 1;
-    return res;
-}
-
 void Transmitir(unsigned char BufferT) {
     while (TRMT == 0);
     TXREG = BufferT;
@@ -176,24 +167,24 @@ unsigned char Recibir(void){
 }
 
 void TransmitirDatos(unsigned int Ent1, unsigned int Ent2) {
-    unsigned int n = Ent1 * 10 + Ent2, TemperaturaC = Temperatura, HumedadC = Humedad;
+    unsigned int n = Ent1 * 10 + Ent2, TempC = Temp, HumC = Hum;
     unsigned int Simb = 67;
-    BorraLCD();
+    //BorraLCD();
     switch (n) {
         case 00://Celsius
-            TemperaturaC = Temperatura;
+            TempC = Temp;
             Simb = 67; //C
             break;
         case 01://Kelvin
-            TemperaturaC = Temperatura + 273;
+            TempC = Temp + 273;
             Simb = 75; //K
             break;
         case 10://Rankine
-            TemperaturaC = Temperatura * 9 / 5 + 492;
+            TempC = Temp * 9 / 5 + 492;
             Simb = 82; //R
             break;
         case 11://Fahrenheit
-            TemperaturaC = Temperatura * 9 / 5 + 32;
+            TempC = Temp * 9 / 5 + 32;
             Simb = 70; //F
             break;
     }
@@ -204,15 +195,15 @@ void TransmitirDatos(unsigned int Ent1, unsigned int Ent2) {
     Transmitir(':');
     Transmitir(' ');
 
-    MensajeLCD_Var("Temperatura:");
-    if (TemperaturaC / 100 > 0) {
-        Transmitir(TemperaturaC / 100 + 48);
-        EscribeLCD_c(TemperaturaC / 100 + 48);
-        TemperaturaC = TemperaturaC % 100;
+    DireccionaLCD(0x85);
+    if (TempC / 100 > 0) {
+        Transmitir(TempC / 100 + 48);
+        EscribeLCD_c(TempC / 100 + 48);
+        TempC = TempC % 100;
     }
-    Transmitir(TemperaturaC / 10 + 48);
-    Transmitir(TemperaturaC % 10 + 48);
-    Transmitir(167);
+    Transmitir(TempC / 10 + 48);
+    Transmitir(TempC % 10 + 48);
+    if (Simb==70 | Simb == 67) Transmitir(223);
     Transmitir(Simb);
     Transmitir(' ');
     Transmitir('\n');
@@ -221,63 +212,57 @@ void TransmitirDatos(unsigned int Ent1, unsigned int Ent2) {
     Transmitir('m');
     Transmitir(':');
     Transmitir(' ');
-    Transmitir(Humedad / 10 + 48);
-    Transmitir(Humedad % 10 + 48);
+    Transmitir(Hum / 10 + 48);
+    Transmitir(Hum % 10 + 48);
     Transmitir(' ');
     Transmitir('%');
     Transmitir('\n');
     Transmitir(' ');
-    
-        
-
-
     //Transmision LCD
-
-
-    EscribeLCD_c(TemperaturaC / 10 + 48);
-    EscribeLCD_c(TemperaturaC % 10 + 48);
+    EscribeLCD_c(TempC / 10 + 48);
+    EscribeLCD_c(TempC % 10 + 48);
+    if (Simb==70 | Simb == 67) EscribeLCD_c(223);
     EscribeLCD_c(Simb);
-    DireccionaLCD(192);
-    MensajeLCD_Var("Humedad:");
-    EscribeLCD_c(Humedad / 10 + 48);
-    EscribeLCD_c(Humedad % 10 + 48);
+    DireccionaLCD(0xC4);
+    EscribeLCD_c(Hum / 10 + 48);
+    EscribeLCD_c(Hum % 10 + 48);
     EscribeLCD_c('%');
 
 }
 
 void ColorRGB(unsigned int val) {
     if (val < 22) {
-        RB7 = 0;
-        RB6 = 0;
         RB5 = 0;
+        RB6 = 0;
+        RB7 = 0;
     } else if (val >= 22 && val < 25) {
-        RB7 = 1;
-        RB6 = 0;
         RB5 = 1;
+        RB6 = 0;
+        RB7 = 1;
     } else if (val >= 25 && val < 28) {
-        RB7 = 0;
-        RB6 = 0;
         RB5 = 1;
+        RB6 = 0;
+        RB7 = 0;
     } else if (val >= 28 && val < 31) {
-        RB7 = 0;
-        RB6 = 1;
         RB5 = 1;
+        RB6 = 1;
+        RB7 = 0;
     } else if (val >= 31 && val < 34) {
+        RB5 = 0;
+        RB6 = 1;
         RB7 = 0;
-        RB6 = 1;
-        RB5 = 0;
     } else if (val >= 34 && val < 37) {
-        RB7 = 1;
-        RB6 = 1;
         RB5 = 0;
-    } else if (val >= 37 && val < 40) {
+        RB6 = 1;
         RB7 = 1;
+    } else if (val >= 37 && val < 42) {
+        RB5 = 0;
         RB6 = 0;
-        RB5 = 0;
-    } else if (val >= 40) {
         RB7 = 1;
-        RB6 = 1;
+    } else if (val >= 42) {
         RB5 = 1;
+        RB6 = 1;
+        RB7 = 1;
     }
 }
 
@@ -288,7 +273,7 @@ unsigned int Conversion(unsigned char canal) {
     return ADRES;
 }
 
-void SaveData_EEPROM(unsigned int dir, unsigned char dato) {
+void GuardarDatoEnEEPROM(unsigned int dir, unsigned char dato) {
     EEADR = dir;
     EEDATA = dato;
     //Configuracion EECON1
@@ -308,7 +293,7 @@ void SaveData_EEPROM(unsigned int dir, unsigned char dato) {
 
 }
 
-unsigned char ReadData_EEPROM(unsigned int dir) {
+unsigned char leerDatoEnEEPROM(unsigned int dir) {
     EEADR = dir; //Establece la direccion a leer
     EEPGD = 0; //Selecciona la memoria EPROM
     CFGS = 0; //Accede a la configuracion de EEPROM
@@ -320,7 +305,7 @@ unsigned char ReadData_EEPROM(unsigned int dir) {
 void __interrupt() ISR(void){
     if(TMR0IF){
         TMR0IF=0;
-        RC0 = !RC0;
+        RE2 = !RE2;
         TMR0 = 3036;//Precarga 2^n - Tsobreflujo*Fbus_Timer0/PreScaler
         //Tuvo que usarse una resolucion de 16 bits y un PS de  para lograr el valor deseado
     }
@@ -345,8 +330,7 @@ void __interrupt() ISR(void){
                 default:
                     break;
         }
-        __delay_ms(100);
-        
-    }
-            
+        __delay_ms(100); 
+    }            
 }
+ 
